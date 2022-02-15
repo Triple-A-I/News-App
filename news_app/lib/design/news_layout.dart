53fa93eb12169171be621bc7437e9b1ffdbd85cc
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-15 12:19:50 -0800

    add blocConsumer , bottomnavigationitems, screens and search icon to newslayout

--- news_app/lib/design/news_layout.dart
+++ news_app/lib/design/news_layout.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/logic/bloc/news_bloc.dart';
 
 class NewsLayout extends StatelessWidget {
   const NewsLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    NewsCubit newsCubit = NewsCubit().get(context);
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('News App'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: newsCubit.currentIndex,
+            onTap: (index) {
+              newsCubit.changeBottomNavBar(index);
+            },
+            items: newsCubit.bottomBarItems,
+          ),
+          body: newsCubit.screens[newsCubit.currentIndex],
+        );
+      },
+    );
   }
 }
