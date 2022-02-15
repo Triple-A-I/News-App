960bf50ee73bdda453bb0d0903e2c85abcfcb134
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-15 10:08:38 -0800

    Create design file which contains news, buisness and sports screen

---
+++ news_app/lib/design/news_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsLayout extends StatelessWidget {
+  const NewsLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
