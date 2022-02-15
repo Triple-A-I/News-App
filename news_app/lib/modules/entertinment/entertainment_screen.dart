c0ee829ec5431d2ce734c55abda930209a524cc0
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-15 12:16:44 -0800

    create entertainment screen class

---
+++ news_app/lib/modules/entertinment/entertainment_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EntertainmnetScreen extends StatelessWidget {
+  const EntertainmnetScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Entertainmnet Screen",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
