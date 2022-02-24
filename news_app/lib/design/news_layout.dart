907db09deb3b32ee460d72597837ecb980b2c05b
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:10:01 -0800

    add change mode function and navigator to search screen

--- news_app/lib/design/news_layout.dart
+++ news_app/lib/design/news_layout.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/logic/bloc/news_bloc.dart';
+import '../logic/bloc/mode_bloc.dart';
+import '../modules/search/search_screen.dart';
+import '../logic/bloc/news_bloc.dart';
 
 class NewsLayout extends StatelessWidget {
   const NewsLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    NewsCubit newsCubit = NewsCubit().get(context);
-    return BlocConsumer<NewsCubit, NewsStates>(
-      listener: (context, state) {},
+    NewsCubit newsCubit = NewsCubit.get(context);
+    ModeCubit modeCubit = ModeCubit.get(context);
+    return BlocBuilder<NewsCubit, NewsStates>(
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
             title: const Text('News App'),
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SearchScreen(),
+                      ));
+                },
                 icon: const Icon(
                   Icons.search,
-                  color: Colors.black,
-                  size: 30,
                 ),
+              ),
+              IconButton(
+                onPressed: () {
+                  modeCubit.changeThemeMode();
+                },
+                icon: const Icon(Icons.brightness_4_outlined),
               ),
             ],
           ),
