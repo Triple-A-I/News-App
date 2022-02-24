95f8bab13a85cee41e6d8af281c83ae0f2e48cc3
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:09:01 -0800

    create all news states

--- news_app/lib/logic/bloc/news_state.dart
+++ news_app/lib/logic/bloc/news_state.dart
@@ -4,4 +4,64 @@ abstract class NewsStates {}
 
 class NewsInitial extends NewsStates {}
 
+class NewsGetBusinessLoadingStates extends NewsStates {}
+
+class NewsGetBusinessSuccessStates extends NewsStates {}
+
+class NewsGetBusinessErrorStates extends NewsStates {
+  final String message;
+
+  NewsGetBusinessErrorStates(this.message);
+}
+
+class NewsGetEntertainmentLoadingStates extends NewsStates {}
+
+class NewsGetEntertainmentSuccessStates extends NewsStates {}
+
+class NewsGetEntertainmentErrorStates extends NewsStates {
+  final String message;
+
+  NewsGetEntertainmentErrorStates(this.message);
+}
+
+class NewsGetSportsLoadingStates extends NewsStates {}
+
+class NewsGetSportsSuccessStates extends NewsStates {}
+
+class NewsGetSportsErrorStates extends NewsStates {
+  final String message;
+
+  NewsGetSportsErrorStates(this.message);
+}
+
+class NewsGetScienceLoadingStates extends NewsStates {}
+
+class NewsGetScienceSuccessStates extends NewsStates {}
+
+class NewsGetScienceErrorStates extends NewsStates {
+  final String message;
+
+  NewsGetScienceErrorStates(this.message);
+}
+
+class NewsGetHealthLoadingStates extends NewsStates {}
+
+class NewsGetHealthSuccessStates extends NewsStates {}
+
+class NewsGetHealthErrorStates extends NewsStates {
+  final String message;
+
+  NewsGetHealthErrorStates(this.message);
+}
+
+class NewsGetSearchLoadingStates extends NewsStates {}
+
+class NewsGetSearchSuccessStates extends NewsStates {}
+
+class NewsGetSearchErrorStates extends NewsStates {
+  final String message;
+
+  NewsGetSearchErrorStates(this.message);
+}
+
 class NewsBottomNavState extends NewsStates {}
