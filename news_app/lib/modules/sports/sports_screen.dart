cf59ac6fef95a8dc9f160483cfd54f991d7b7c1d
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:21:11 -0800

    add bloc consumer to sports screen

--- news_app/lib/modules/sports/sports_screen.dart
+++ news_app/lib/modules/sports/sports_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../design/widgets/article_builder.dart';
+import '../../logic/bloc/news_bloc.dart';
 
 class SportsScreen extends StatelessWidget {
   const SportsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Sports Screen",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {
+        if (state is NewsGetSportsErrorStates) {}
+      },
+      builder: (context, state) {
+        var list = NewsCubit.get(context).sports;
+        return articleBuilder(state, NewsGetSportsLoadingStates(), list);
+      },
     );
   }
 }
