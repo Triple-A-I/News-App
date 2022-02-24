a2c678f3b86fde122402d322be508d1517c5ad21
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:18:04 -0800

    change some style in default form field

--- news_app/lib/design/widgets/default_from_field.dart
+++ news_app/lib/design/widgets/default_from_field.dart
@@ -4,26 +4,27 @@ Widget defaultFormField({
   required TextEditingController controller,
   required TextInputType type,
   String label = '',
-  required FormFieldValidator validate,
   required Function(String value) onChanged,
   IconData? icon,
+  required FormFieldValidator<String> validator,
 }) =>
     TextFormField(
-      style: TextStyle(color: Colors.white),
       onChanged: onChanged,
       controller: controller,
       keyboardType: type,
-      validator: validate,
+      validator: validator,
       decoration: InputDecoration(
-          prefixIcon: Icon(
-            icon,
+        labelText: label,
+        border: const OutlineInputBorder(),
+        prefixIcon: Icon(
+          icon,
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(5),
           ),
-          enabledBorder: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(5),
-            ),
-          ),
-          errorStyle: const TextStyle(color: Colors.red),
-          hintText: label,
-          filled: true),
+        ),
+        errorStyle: const TextStyle(color: Color.fromRGBO(244, 67, 54, 1)),
+        filled: true,
+      ),
     );
