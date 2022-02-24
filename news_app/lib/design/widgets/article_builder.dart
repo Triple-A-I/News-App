6d9f8fedee5a47549461ddac87fbbe3aada2c071
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:17:15 -0800

    rearrange article builder

--- news_app/lib/design/widgets/article_builder.dart
+++ news_app/lib/design/widgets/article_builder.dart
@@ -6,15 +6,16 @@ import 'article_item.dart';
 import 'my_divider.dart';
 
 Widget articleBuilder(NewsStates state, NewsStates loading, list) {
-  return state != loading
-      ? ListView.separated(
+  print("ALBRAAAAAAAAAAAAAAA:  $loading");
+  return state == loading
+      ? const Center(
+          child: CircularProgressIndicator(),
+        )
+      : ListView.separated(
           physics: const BouncingScrollPhysics(),
           itemBuilder: (context, index) =>
               buildArticleItem(list[index], context),
           separatorBuilder: (context, index) => myDivider(),
           itemCount: list.length,
-        )
-      : const Center(
-          child: CircularProgressIndicator(),
         );
 }
