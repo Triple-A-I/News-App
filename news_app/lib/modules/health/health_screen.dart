379938d6d3f04c0b707579709ca770d4682e4cbd
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:20:42 -0800

    add bloc consumer to health screen

--- news_app/lib/modules/health/health_screen.dart
+++ news_app/lib/modules/health/health_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../design/widgets/article_builder.dart';
+import '../../logic/bloc/news_bloc.dart';
 
 class HealthScreen extends StatelessWidget {
   const HealthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Health Screen",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {
+        if (state is NewsGetHealthErrorStates) {}
+      },
+      builder: (context, state) {
+        var list = NewsCubit.get(context).health;
+        return articleBuilder(state, NewsGetHealthLoadingStates(), list);
+      },
     );
   }
 }
