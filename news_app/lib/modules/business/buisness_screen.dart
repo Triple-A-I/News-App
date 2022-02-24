76c800c9314bab5757a90ac3bfb23a77b53d2755
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-02-24 11:15:55 -0800

    add bloc consumer and article builder to business screen

--- news_app/lib/modules/business/buisness_screen.dart
+++ news_app/lib/modules/business/buisness_screen.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../design/widgets/article_builder.dart';
+
+import '../../logic/bloc/news_bloc.dart';
 
 class BusinessScreen extends StatelessWidget {
   const BusinessScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Business Screen",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {
+        if (state is NewsGetBusinessErrorStates) {}
+      },
+      builder: (context, state) {
+        var list = NewsCubit.get(context).business;
+        return articleBuilder(state, NewsGetBusinessLoadingStates(), list);
+      },
     );
   }
 }
